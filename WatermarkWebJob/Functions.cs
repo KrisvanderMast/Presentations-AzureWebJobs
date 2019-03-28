d3db51b3aa136b7634a4b4e73a30c796347388e0
Kris van der Mast <kris.vandermast@gmail.com>
2019-03-28 16:50:31 +0100

    Working application - just need to change the connectionstrings to your storageaccount

---
+++ WatermarkWebJob/Functions.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using Microsoft.Azure.WebJobs;
+
+namespace WatermarkWebJob
+{
+    public class Functions
+    {
+        public static void WebImageResize(
+            [BlobTrigger("inputimages/{name}")] WebImage input,
+            [Blob("outputimages/{name}")] out WebImage output)
+        {
+            output = input.Resize(500, 500).AddTextWatermark("VaHa bvba", fontColor: "Red");
+        }
+    }
+
+    public class WebImageBinder : ICloudBlobStreamBinder<WebImage>
+    {
+        public Task<WebImage> ReadFromStreamAsync(Stream input, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<WebImage>(new WebImage(input));
+        }
+
+        public Task WriteToStreamAsync(WebImage value, Stream output, CancellationToken cancellationToken)
+        {
+            var bytes = value.GetBytes();
+            return output.WriteAsync(bytes, 0, bytes.Length, cancellationToken);
+        }
+    }
+}
